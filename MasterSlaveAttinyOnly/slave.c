5991fa2da22a2d2ad54751ddc1c06023bbc2885c
simonGoelles <goelle190028@sr.htlweiz.at>
2023-05-16 10:24:54 +0200

    yes

--- MasterSlaveAttinyOnly/slave.c
+++ MasterSlaveAttinyOnly/slave.c
@@ -1,36 +1,47 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/i2c.h>
 
-char received[200];
+#define SLAVE_ADDRESS 0x10
 
-void spi_setup()
+volatile uint8_t received_data;
+
+void i2c_setup(uint8_t address)
 {
-    DDRB = (1 << PB1) | (1 << PB3);  // set MOSI and SCK as output
-    USICR = (1 << USIWM0);          // enable three-wire mode (SPI)
+    TWAR = (address << 1); // Set slave address
+    TWCR = (1 << TWEN) | (1 << TWEA); // Enable I²C and enable ACK
+}
+
+void i2c_receive()
+{
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+    while (!(TWCR & (1 << TWINT)));
+    received_data = TWDR;
+}
+
+void i2c_send(uint8_t data)
+{
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while (!(TWCR & (1 << TWINT)));
 }
 
 int main()
 {
-    spi_setup();
-
+    i2c_setup(SLAVE_ADDRESS);
+    sei(); // Enable global interrupts
+    
     while (1) {
-        // wait for a command
-        int i = 0;
-        while (1) {
-            received[i] = USIBR;
-            if (USISR & (1 << USIOIF)) { // check for end of transmission
-                USISR |= (1 << USIOIF); // clear counter overflow flag
-                break;
+        // Wait for I²C address match and data received
+        while (!(TWCR & (1 << TWINT)));
+        if ((TWSR & 0xF8) == TW_SR_SLA_ACK) { // Address match
+            i2c_receive(); // Receive data
+            if (received_data == 0x01) {
+                // Turn on LED
+                // Code to turn on the LED goes here
             }
-            i++;
         }
 
-        // turn on LED if command is to turn on
-        if (received[0] == 0x01) {
-            PORTB |= (1 << PB3); // turn on LED
-            _delay_ms(200);
-            PORTB &= ~(1 << PB3); // turn off LED
-            _delay_ms(400);
-        }
+        TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA); // Enable ACK
     }
 }
